ecdfe69b8c4945345e86bc5169286418d28e19e0
adeha047 <dehaven7777@gmail.com>
2021-03-01 16:28:58 -0500

    added new files for data flowing purposes

--- server.js
+++ server.js
@@ -1,40 +1,31 @@
-const express = require('express');
-const path = require ('path');
+var express = require("express");
+
 const app = express();
-const fs = require("fs");
-const { response } = require('express');
 
-const PORT = 3000;
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const notes = []; 
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-app.get("/index", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
-});
-
-app.post("/api/notes", (req, res) => {
-    let data = req.body;
-    console.log(data)
-    notes.push(data)
-    console.log(notes)
-    res.sendFile(path.join(__dirname, "db/db.json"));
-    res.json(data)
-
-}); 
-
-
-
-
-app.listen(PORT, function () {
-    console.log("Server is listening on PORT: " + PORT);
-})
 
 
 // The following HTML routes should be created:

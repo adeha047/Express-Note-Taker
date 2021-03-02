33dd53d0d1cd686546acb7b19a2c57d8bb85af38
adeha047 <dehaven7777@gmail.com>
2021-03-01 19:32:33 -0500

    added new updated calls in apiRoute.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,8 @@
+const database = require("../db/db.json")
+
+const fs = require("fs");
+
+
 module.exports = function(app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
@@ -10,3 +15,10 @@ module.exports = function(app) {
     });
   
 }  
+
+
+// * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+
+// * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+// * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
